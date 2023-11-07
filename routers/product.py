c1d4845d203da42fb1f7694d9e0d6e074c1e9d25
EIvan21 <edher.diaz21@gmail.com>
2023-11-06 23:05:20 -0600

    adding new endpoints, get products by type, madera, delete product

--- routers/product.py
+++ routers/product.py
@@ -4,8 +4,79 @@ from config.database import Session
 from services.product import ProductService
 from fastapi.responses import  JSONResponse
 from fastapi.encoders import jsonable_encoder
+from fastapi import  HTTPException, status, Path, Query, Depends
 
 products_router = APIRouter()
+
+
+#-------------------GETTING ALL PRODUCTS -----------------------------
+
+@products_router.get('/productos',
+                     tags=["Productos"],
+                     status_code=200,
+                     summary="Todos los productos")
+async def obtener_productos():
+    db = Session()
+    result = ProductService(db).get_products()
+    return JSONResponse(content=jsonable_encoder(result))
+
+#---------------GETING PRODUCT BY ID ----------------------------------
+
+@products_router.get(path='/productos/read/{id}',
+                     tags=["Productos"],
+                     status_code=status.HTTP_200_OK,
+                     summary="Product by id"                     
+                     )
+async def get_movie_by_id(id:int = Path(ge=1, le=2000)):
+    
+    """
+    Obtener el producto por id por parametro de ruta
+    """
+
+    db = Session()
+    result = ProductService(db).get_product_by_id(id)
+
+    if not result:
+        raise HTTPException(status_code=404, detail="Id product not found")
+
+    return JSONResponse(status_code=200, content=jsonable_encoder(result))
+
+#------------------- GETTING PRODUCT BY TYPE or MADERA ----------------------
+
+@products_router.get(path="/productos/read/",
+                     tags=["Productos"],
+                     status_code=status.HTTP_200_OK,
+                     summary="Producto por tipo o madera"
+                     )
+async def get_product_by_type_or_madera(tipo: str = Query(min_length=1, max_length=15, default=None), madera:str = Query(default=None)):
+
+
+    """Obtener productos por el tipo de material (Tabla, Tablero, Construcción)"""
+
+    db = Session()
+
+    if tipo is None and madera is None:
+        raise HTTPException(status_code=404, detail="Escribe algun tipo o madera del producto")
+    
+    elif tipo is None:
+        result = ProductService(db).get_product_by_madera(madera)
+        if not result:
+            raise HTTPException(status_code=404, detail="Madera no encontrada")
+        
+    
+    elif madera is None:
+        result = ProductService(db).get_product_by_type(tipo)
+        if not result:
+            raise HTTPException(status_code=404, detail="Tipo de producto no encontrado")
+    else:
+
+        result = ProductService(db).get_product_by_type_madera(tipo,madera)
+        if not result:
+            raise HTTPException(status_code=404, detail="Tipo de producto no encontrado")
+        
+    return JSONResponse(status_code=200, content=jsonable_encoder(result))
+
+#------------------CREATING A NEW PRODUCT ------------------
 
 @products_router.post("/productos/create/", 
                       tags=["Productos"],
@@ -17,12 +88,23 @@ async def crear_producto(producto: ProductoCreate):
     db = Session()
     ProductService(db).create_product(producto)
     return JSONResponse(content={"message": "Se ha creado el producto: " + producto.nombre })
+    
+#------------ DELETING PRODUCT -----------------------------------------------------
 
-@products_router.get('/productos',
-                     tags=["Productos"],
-                     status_code=200,
-                     summary="Todos los productos")
-async def obtener_productos():
+@products_router.delete(path='/delete/{id}',
+                        tags=["Productos"],
+                        status_code=status.HTTP_200_OK,
+                        summary="Delete product"
+                        
+                        )
+async def delete_product(id: int):
+
+    """ Eliminar pelicula por id """
+
     db = Session()
-    result = ProductService(db).get_products()
-    return JSONResponse(content=jsonable_encoder(result))
+    result = ProductService(db).get_product_by_id(id)
+
+    if not result:
+        raise HTTPException(status_code=404, detail="Producto no encontrado")
+    else:
+        ProductService(db).delete_producto(id)
--- services/product.py
+++ services/product.py
@@ -7,6 +7,29 @@ class ProductService():
 
         self.db = db
     
+    
+    def get_products(self):
+        result = self.db.query(ProductoModel).all()
+        return result
+
+    def get_product_by_id(self, id):
+        result = self.db.query(ProductoModel).filter(ProductoModel.id == id).first()
+        return result
+    
+    def get_product_by_type(self, tipo):
+        result = self.db.query(ProductoModel).filter(ProductoModel.tipo == tipo).all()
+        return result
+    
+    def get_product_by_madera(self, madera):
+        result = self.db.query(ProductoModel).filter(ProductoModel.madera == madera).all()    
+        return result
+    
+    def get_product_by_type_madera(self, tipo, madera):
+
+        result = self.db.query(ProductoModel).filter(ProductoModel.tipo == tipo).filter(ProductoModel.madera == madera).all()
+
+        return result
+    
     def create_product(self, product: Producto):
 
         new_product = ProductoModel(**product.dict())
@@ -14,7 +37,19 @@ class ProductService():
         self.db.commit()
         return
     
-    def get_products(self):
+    def update_producto(self, id: int, data: Producto):
+        producto = self.db.query(ProductoModel).filter(ProductoModel.id == id)
 
-        result = self.db.query(ProductoModel).all()
-        return result
+        producto_dict = data.dict()
+        producto_dict['id'] = id
+
+        producto.update(producto_dict, synchronize_session = False)
+        self.db.commit()
+        return
+    
+    def delete_producto(self, id:int):
+        producto = self.db.query(ProductoModel).filter(ProductoModel.id == id).first()
+
+        self.db.delete(producto)
+        self.db.commit()
+        return
